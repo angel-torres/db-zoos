0f13b8171b4d53ff9ce91fc4d6b56741bd12deb1
Angel Torres <alas4224@gmail.com>
2019-02-19 18:12:41 -0600

    added get and post endpoints

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true, //needed for sqlite
+  connection: {
+    filename: './data/lambda.sqlite3'
+  }
+}
+const db = knex(knexConfig);
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get('/api/zoos', async(req, res) => {
+  try {
+    const zoos = await db('zoos');
+    res.status(200).json(zoos)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+server.get('/api/zoos/:id', async(req, res) => {
+  try {
+    const zoo = await db('zoos').where({ id: req.params.id }).first('name');
+    res.status(200).json(zoo)
+  } catch (error) {
+  }
+})
+
+server.post('/api/zoos', async (req, res) => {
+  try {
+    const [id] = await db('zoos').insert(req.body);
+    const zoo = await db('zoos').where({ id }).first();
+    res.status(201).json(zoo)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 
 const port = 3300;
 server.listen(port, function() {
